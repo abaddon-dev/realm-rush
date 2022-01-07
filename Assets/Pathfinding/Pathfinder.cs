6aacf081534270cdeb3bf9632a64982ca0394f7d
abaddon-dev <pawel.dwornicki86@gmail.com>
2022-01-07 20:28:47 +0100

    Added setting on the script execution order.

--- Assets/Pathfinding/Pathfinder.cs
+++ Assets/Pathfinding/Pathfinder.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Pathfinder : MonoBehaviour
 {
     [SerializeField] Vector2Int startCoordinates;
+    public Vector2Int StartCoordinates { get { return startCoordinates; } }
+
     [SerializeField] Vector2Int destinationCoordinates;
+    public Vector2Int DestinationCoordinates { get { return destinationCoordinates; } }
+
     Node startNode;
     Node destinationNode;
     Node currentSearchNode;
@@ -22,13 +26,12 @@ public class Pathfinder : MonoBehaviour
         if(gridManager != null)
         {
             grid = gridManager.Grid;
-        }
+            startNode = grid[startCoordinates];
+            destinationNode = grid[destinationCoordinates];
+        }    
     }
     void Start()
     {
-        startNode = gridManager.Grid[startCoordinates];
-        destinationNode = gridManager.Grid[destinationCoordinates];
-
         GetNewPath();
     }
 
@@ -66,6 +69,9 @@ public class Pathfinder : MonoBehaviour
 
     void BreadthFirstSearch()
     {
+        startNode.isWalkable = true;
+        destinationNode.isWalkable = true;
+        
         frontier.Clear();
         reached.Clear();
 
